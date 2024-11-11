c48f186c39762d568660fb2c13c8fe4a044e1679
CristhianAc <cristhian_agamez@outlook.com>
2024-11-11 00:54:48 -0500

    refactor: Clean up app component HTML and improve formatting in TypeScript file

--- EscalaFrontend/src/app/app.component.ts
+++ EscalaFrontend/src/app/app.component.ts
@@ -11,55 +11,55 @@ import { StatesService } from './services/states.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, AsideBarComponent, SelectButtonModule, FormsModule, LoadingComponent],
+  imports: [
+    RouterOutlet,
+    AsideBarComponent,
+    SelectButtonModule,
+    FormsModule,
+    LoadingComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  
   statesManager = inject(StatesService);
   disabled = this.statesManager.disabled;
-  loadingState = false ;
+  loadingState = false;
   stateOptions: any[] = [
     { label: 'Map', value: 'map' },
-    { label: 'Graphs', value: 'graph' }
+    { label: 'Graphs', value: 'graph' },
   ];
   router = inject(Router);
-  value: string = 'map'; 
+  value: string = 'map';
   ngOnInit(): void {
     this.statesManager.loadingState$.subscribe((state: boolean) => {
       this.loadingState = state;
     });
-    
   }
-  
-  constructor() {
-  
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      if(event.url === '/'){
-        this.statesManager.disabled.set(false);
-      } 
 
-      if (event.url === '/map') {
-        this.value = 'map';
-        this.statesManager.disabled.set(true);
-      } else if (event.url === '/dashboard') {
-        this.value = 'graph';
-        this.statesManager.disabled.set(true);
-      }
-      
-    });
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (event.url === '/') {
+          this.statesManager.disabled.set(false);
+        }
+
+        if (event.url === '/map') {
+          this.value = 'map';
+          this.statesManager.disabled.set(true);
+        } else if (event.url === '/dashboard') {
+          this.value = 'graph';
+          this.statesManager.disabled.set(true);
+        }
+      });
   }
-  
+
   onStateChange() {
     if (this.value === 'map') {
       this.router.navigate(['/map']);
     } else if (this.value === 'graph') {
-      this.router.navigate(['/dashboard']); 
+      this.router.navigate(['/dashboard']);
     }
-   
   }
 }
-
