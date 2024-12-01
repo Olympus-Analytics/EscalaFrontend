96578db53672312317075f072498218c087152d9
CristhianAc <cristhian_agamez@outlook.com>
2024-12-01 16:41:54 -0500

    feat: Move year selection from aside bar to graph component and update related logic

--- EscalaFrontend/src/app/modules/graphs/components/graph/graph.component.ts
+++ EscalaFrontend/src/app/modules/graphs/components/graph/graph.component.ts
@@ -7,18 +7,40 @@ import { FormsManagersService } from '../../../../services/forms-managers.servic
 import { Datasets } from '@/models/graph.model';
 import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [ChartModule, MatFormFieldModule, MatSelectModule, TooltipModule],
+  imports: [
+    ChartModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    TooltipModule,
+    FormsModule,
+    CalendarModule,
+  ],
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.css'],
 })
 export class GraphComponent implements OnInit {
   dataService = inject(DataService);
   formControlService = inject(FormsManagersService);
+  years: { name: string; code: string }[] = [
+    { name: '2016', code: '2016' },
+    { name: '2017', code: '2017' },
+    { name: '2018', code: '2018' },
+    { name: '2019', code: '2019' },
+    { name: '2020', code: '2020' },
+    { name: '2021', code: '2021' },
+    { name: '2022', code: '2022' },
+  ];
 
+  minDate: Date = new Date(2016, 0, 1);
+  maxDate: Date = new Date(2022, 11, 31);
+  rangeDates = this.formControlService.rangeDates;
+  dateRaster = this.formControlService.dateRaster;
   endpoints = [
     {
       name: 'Traffic Collisions Count',
